8baf39b3ada6bbcf83b94317aceacd3614cff6c4
Lechao Diwu <wudi@microsoft.com>
2014-07-26 00:19:54 +0800

    add flurry add chinese

--- src/com/wudi/wechatshareeditor/MainActivity.java
+++ src/com/wudi/wechatshareeditor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wudi.wechatshareeditor;
 
+import com.flurry.android.FlurryAgent;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -17,7 +20,7 @@ public class MainActivity extends Activity {
 			uploadHeightEditText, uploadSouceEditText;
 	public String title, image_url, source_display_username, source_username,
 			upload_height, upload_link, upload_source, upload_width;
-	private Button confirm;
+	private Button confirm, launch;
 	private SettingsHelper settingsHelper;
 
 	@Override
@@ -45,6 +48,8 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
+				FlurryAgent.logEvent("Save");
+
 				title = titleEditText.getText().toString();
 				settingsHelper.setString("title", title);
 
@@ -71,8 +76,23 @@ public class MainActivity extends Activity {
 				upload_width = uploadWidthEditText.getText().toString();
 				settingsHelper.setString("upload_width", upload_width);
 
-				Toast.makeText(context, "Done", Toast.LENGTH_LONG).show();
+				Toast.makeText(context, R.string.toast, Toast.LENGTH_LONG)
+						.show();
 			}
+		});
+
+		launch = (Button) this.findViewById(R.id.launch);
+
+		launch.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				FlurryAgent.logEvent("Launch_wechat");
+				Intent LaunchIntent = getPackageManager()
+						.getLaunchIntentForPackage("com.tencent.mm");
+				startActivity(LaunchIntent);
+			}
+
 		});
 	}
 
@@ -97,6 +117,18 @@ public class MainActivity extends Activity {
 		uploadLinkEditText.setText(upload_link);
 		uploadSouceEditText.setText(upload_source);
 		uploadWidthEditText.setText(upload_width);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		FlurryAgent.onStartSession(this, "M8KFV7ZV3VT97QJTQDMZ");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		FlurryAgent.onEndSession(this);
 	}
 
 }
